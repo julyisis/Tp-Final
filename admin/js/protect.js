4eba7628d47afe43ffca2d4dde5f8f03a76a0526
Garo <94136201+MagoGaro@users.noreply.github.com>
2025-06-16 18:39:00 -0300

    Update protect.js
    otro ajuste de rutas

--- admin/js/protect.js
+++ admin/js/protect.js
@@ -2,7 +2,7 @@ const user = JSON.parse(localStorage.getItem("user"));
 const token = localStorage.getItem("accessToken");
 
 if (user == null || !token) {
-            window.location.href = "../../login.html";
+            window.location.href = "../login.html";
 }else{    
      document.getElementById("userInfo").textContent = `Bienvenid@, ${user.firstName}`;
 }
